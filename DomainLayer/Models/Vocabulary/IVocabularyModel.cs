bce35218fd0c7cb6d96c34cdb31b3987dc03a2a3
Matricarin <matricarin@gmail.com>
2023-02-04 10:37:44 +0500

    Create model andmodel interface.

---
+++ DomainLayer/Models/Vocabulary/IVocabularyModel.cs
@@ -0,0 +1,10 @@
+﻿namespace DomainLayer.Models.Vocabulary
+{
+    public interface IVocabularyModel
+    {
+        string EngWord { get; set; }
+        string PartOfSpeech { get; set; }
+        string RusWord { get; set; }
+        int WordId { get; set; }
+    }
+}
---
+++ DomainLayer/Models/Vocabulary/VocabularyModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainLayer.Models.Vocabulary
+{
+    public class VocabularyModel : IVocabularyModel
+    {
+        public int WordId { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessage = "English word is required field.")]
+        [StringLength(40, MinimumLength = 2, ErrorMessage = "Word or phrase must be between 3 and 40 characters")]
+        public string EngWord { get; set; }
+        [Required(ErrorMessage = "Part of speech is required field.")]
+        [StringLength(4, MinimumLength = 1, ErrorMessage = "Must be abbreviation of the part of speech")]
+        [RegularExpression(@"(n|v|adj|prep|adv|pron|conj)", ErrorMessage = "Incorrect format")]
+        public string PartOfSpeech { get; set; }
+        [Required(ErrorMessage = "Russian word is required field.")]
+        [StringLength(40, MinimumLength = 2, ErrorMessage = "Word or phrase must be between 3 and 40 characters")]
+        public string RusWord { get; set; }
+    }
+}
